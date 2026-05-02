80478bbfeea4316a12ddbeec3831001a4b08d6fd
Hanieh65 <hanieh.a65@gmail.com>
2026-05-02 21:48:36 +0100

    Add files via upload

---
+++ netflix-content-analysis/sql/Netflix Content Analysis (Sample).sql
@@ -0,0 +1,95 @@
+SELECT * FROM netflix.netflix_titles;
+
+CREATE TABLE netflix_stagging AS 
+SELECT * FROM netflix_titles;
+-- FIND duplicate and delete 
+SELECT *, 
+row_number() OVER(PARTITION BY type,title,director,cast,country,country,date_added,release_year,rating,duration,listed_in)
+FROM  netflix_stagging;
+
+WITH netflix_stagging AS 
+(SELECT *, 
+row_number() OVER(PARTITION BY type,title,director,cast,country,country,date_added,release_year,rating,duration,listed_in) 
+as row_num
+FROM  netflix_titles )
+SELECT *
+FROM netflix_stagging 
+WHERE row_num>1;
+-- 
+SELECT date_added,
+STR_TO_DATE(date_added,'%M %d,%Y') AS clean_date_added
+FROM netflix_stagging;
+ 
+ SELECT * 
+ FROM netflix_stagging
+ WHERE STR_TO_DATE( date_added,'%M %d,%Y') IS NULL;
+ 
+ UPDATE netflix_stagging
+ SET date_added = STR_TO_DATE( date_added,'%M %d,%Y') 
+ WHERE STR_TO_DATE( date_added,'%M %d,%Y') IS NOT NULL;
+  
+  ALTER  TABLE netflix_stagging 
+  MODIFY date_added DATE;
+  
+ UPDATE netflix_stagging
+ SET type = CASE 
+                WHEN duration LIKE '%min%' THEN 'movie'
+                WHEN duration LIKE '%season%' THEN 'Tv show'
+                ELSE type 
+END 
+WHERE type IS NULL;
+
+ SELECT
+     CASE 
+       WHEN duration LIKE '%min%' THEN 'movie'
+       WHEN duration LIKE '%season%' THEN 'TV show'
+     END  AS duration_type,
+   SUBSTRING_INDEX(duration , ' ' ,1) AS duration_value
+	 FROM netflix_stagging;
+     
+     ALTER TABLE netflix_stagging
+     ADD COLUMN duration_value INT;
+     
+     UPDATE netflix_stagging 
+     SET duration_value=CAST( SUBSTRING_INDEX(TRIM(duration) , ' ' ,1) AS UNSIGNED)
+     WHERE duration IS NOT NULL
+      AND TRIM(duration) != ' ';
+ -- total_content_available_on_Netflix
+ SELECT COUNT(*)
+ FROM netflix_stagging;
+ 
+ -- top_10_release_year 
+ SELECT COUNT(*)  AS total,release_year
+ FROM netflix_stagging
+ GROUP BY release_year
+ ORDER BY release_year DESC 
+ LIMIT 10;
+ 
+ -- top_10_type_shows
+ SELECT type, COUNT(*)*100.0 /SUM(COUNT(*)) OVER() AS percentage
+ FROM netflix_stagging
+ GROUP BY type 
+ ORDER BY percentage DESC;
+ 
+ -- top_10_genre 
+ SELECT listed_in , COUNT(*) AS TOTAL
+ FROM netflix_stagging
+ GROUP BY listed_in
+ ORDER BY TOTAL DESC 
+ LIMIT 10;
+ 
+ -- TREND 
+ WITH yearly_counts AS(
+      SELECT 
+            release_year,
+            COUNT(*) AS total
+	  FROM netflix_stagging
+      GROUP BY release_year
+      )
+SELECT 
+      release_year,total,
+      ROUND((total-LAG(total) OVER(ORDER BY release_year))*100.0/
+      LAG(total) OVER(ORDER BY release_year),2) AS growth_percentage
+FROM yearly_counts
+ORDER BY release_year;
+ 
